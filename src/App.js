84ecedffc6aba60351f7bf5cc1027e11bb614b0b
indhu yelineni <indhuyelineni@indhus-air.lan>
2022-11-26 17:53:59 -0500

    Topbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import {Topbar} from "./scenes/global/Topbar";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,6 @@
-import { Box,IconButton,useTheme } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
-import { ColorModeContext,tokens } from "../../theme";
+import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
@@ -9,35 +9,47 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 
-const Topbar = () =>{
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
 
-    //only Box component has this feature of writing the css properties directly
-    return <Box display="flex" justifyContent="space-between" p={2}>
-        {/* Search Bar */}
-        <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
-            <InputBase sx={{ml:2,flex:1}} placeholder="Search"/>
-            <IconButton type="button" sx={{p:1}}>
-            <SearchIcon/>
-            </IconButton>
-        </Box>
+  //only Box component has this feature of writing the css properties directly
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* Search Bar */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
 
-        {/* Icons */}
+      {/* Icons */}
+      <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
-            {theme.pallete.mode === "dark" ? (<DarkModeOutlinedIcon/>):(<LightModeOutlinedIcon/>)}     
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
         </IconButton>
         <IconButton>
-            <NotificationsOutlinedIcon/>
+          <NotificationsOutlinedIcon />
         </IconButton>
         <IconButton>
-            <SettingsOutlinedIcon/>
+          <SettingsOutlinedIcon />
         </IconButton>
         <IconButton>
-            <PersonOutlinedIcon/>
+          <PersonOutlinedIcon />
         </IconButton>
+      </Box>
     </Box>
-}
+  );
+};
 
 export default Topbar;
