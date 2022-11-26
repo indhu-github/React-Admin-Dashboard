5d9cbf61043c4472d5ada47aa50204eac9590830
indhu yelineni <indhuyelineni@indhus-air.lan>
2022-11-26 01:19:03 -0500

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import {Topbar} from "./scenes/global/Topbar";
+
 function App() {
   const [theme, colorMode] = useMode();
-  
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar/>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
