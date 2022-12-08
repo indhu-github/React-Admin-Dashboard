ee9bdd876136b1277d9fad871f01d6ae6103288b
indhu yelineni <indhusworkmail@gmail.com>
2022-12-08 14:08:22 -0500

    Reusable Header Component

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+    return(
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+            </Box>
+        </Box>
+    )
+}
+export default Dashboard;
