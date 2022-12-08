b87bb6224d4c47c6d38e6f4165bd1173beeaf142
indhu yelineni <indhuyelineni@indhus-air.lan>
2022-12-07 23:52:27 -0500

    Sidebar with React Pro Sidebar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
+// import Dashboard from "./scenes/dashboard";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
@@ -27,7 +27,7 @@ function App() {
           <main className="content">
             <Topbar/>
             <Routes>
-              <Route path="/" element={<Dashboard/>}/>
+              {/* <Route path="/" element={<Dashboard />} /> */}
               {/* <Route path="/team" element={<Team/>}/> */}
               {/* <Route path="/contacts" element={<Contacts/>}/> */}
               {/* <Route path="/invoices" element={<Invoices/>}/> */}
