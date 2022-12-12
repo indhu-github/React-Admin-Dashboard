075c18d6c28a3c59b4b8312ae0e745dcddb20504
indhu yelineni <indhusworkmail@gmail.com>
2022-12-12 17:24:58 -0500

    gh pages

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,7 +107,7 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Shershaah
+                  Julie
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   Admin
